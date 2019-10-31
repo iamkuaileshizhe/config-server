cfd4f2214b3c1d9af59e869542b175dd64aab9b5
Trying <kuaileshizhe_01@126.com>
2019-10-31 17:28:36 +0800

    Initial commit

---
+++ src/main/java/com/trying/Application.java
@@ -0,0 +1,29 @@
+package com.trying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+* @Title: Application
+* @Description:  启动类 和 Controller 有共同的父包 ,如果 启动类 和 controller 没有共同的父包，则需要在启动上增加@ComponentScan注解
+* @author huxx
+* @date 2019/10/30 下午3:19
+* @update
+*/
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class Application {
+	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setWebApplicationType(WebApplicationType.SERVLET);
+		app.run(args);
+
+	}
+
+}
